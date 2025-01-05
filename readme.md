91f55c435656cefb413baa0832e53d5215560bdd
Olhalvo <bernardomeloesteves1234@gmail.com>
2025-01-05 22:33:06 +0000

    initial commitar -xf jetbrains-toolbox-2.5.2.35332.tar.gz

---
+++ readme.md
@@ -0,0 +1,10 @@
+# CHIOS
+### A simple 32 bit OS
+
+Made following nanobyte's [Building an OS](https://www.youtube.com/watch?v=9t-SPC7Tczc&list=PLFjM7v6KGMpiH2G-kT781ByCNC_0pKpPN) series.
+
+Named after my gf :3
+
+I luv u Chichi
+
+
---
+++ src/main.asm
@@ -0,0 +1,56 @@
+org 0x7c00     ;load into bootable sector
+bits 16     
+
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+;
+; @author Olhalvo
+; prints a string to the screen
+; @param ds:si points to string untill null character
+;
+puts:
+    push si
+    push ax
+    
+
+.loop:
+    lodsb
+    or al,al
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0x0
+    int 0x10
+    
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+        
+
+main:
+    
+    mov ax, 0
+    mov es, ax
+    mov ds, ax
+
+    mov ss, ax
+    mov sp, 0x7c00
+
+    mov si, msg_hello
+    call puts
+    
+    hlt
+
+.halt:
+    jmp .halt;
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+times 510-($-$$)db 0;
+dw 0AA55h
